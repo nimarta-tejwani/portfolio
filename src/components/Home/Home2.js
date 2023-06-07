eedbf66adc5004bdd4312ec086cd9628152ab891
Nimarta <nimarta.bcss19@iba-suk.edu.pk>
2023-06-08 01:02:10 +0500

    add projects

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -20,36 +20,35 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I'm a fresh graduate in computer science with immense enthusiasm in 
               <br />
-              <br />I am fluent in classics like
+              {/* <br />I am fluent in classics like */}
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> Artificial Intelligence, Machine Learning, and Computer Vision. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">AI and Machine Learning </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  Deep Learning and Computer Vision.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              I also apply my passion for developing products
+              with <b className="purple">Python</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  Modern Library and Frameworks
                 </b>
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> Tensorflow, PyTorch, Keras, SkLearn</b>
               </i>
             </p>
           </Col>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import aerial from "../../Assets/Projects/aerial.PNG";
+import chatbot from "../../Assets/Projects/chatbot.PNG";
+import fake_news from "../../Assets/Projects/fake_news.PNG";
+import flower from "../../Assets/Projects/flower.png";
+import fruit from "../../Assets/Projects/fruit.png";
 import emojify from "../../Assets/Projects/emojify.png";
 import helmet from "../../Assets/Projects/helmet.PNG";
 import vehicle from "../../Assets/Projects/vehicle.png";
@@ -12,10 +14,10 @@ import vehicle from "../../Assets/Projects/vehicle.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
-      <Particle />
+      {/* <Particle /> */}
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Recent <strong className="purple">Projects </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
@@ -26,7 +28,7 @@ function Projects() {
               imgPath={emojify}
               isBlog={false}
               title="Emojify"
-              description="Facial Emotion mapping to Emoticon using DeepFace and Flask"
+              description="Realtime Facial Emotion mapping to Emoticon using DeepFace and Flask"
               ghLink="https://github.com/nimarta-devi/Emojify"
               demoLink=""
             />
@@ -43,16 +45,6 @@ function Projects() {
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col> */}
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -67,26 +59,61 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={flower}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Flower Classification"
+              description="Flower classification using CNN"
+              ghLink="https://github.com/nimarta-devi/flower_classification"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={fruit}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Fruit Calories Estimation"
+              description="Fruit classification using CNN model and calorie estimation"
+              ghLink="https://github.com/nimarta-devi/Fruit_Calories_Estimation"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={aerial}
+              isBlog={false}
+              title="Aerial Image Classification"
+              description="Aerial Image Classification using ResNet50 model (transfer learning)"
+              ghLink="https://github.com/nimarta-devi/aerial_image_classification"
+              demoLink=""              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatbot}
+              isBlog={false}
+              title="Pharmacy Chatbot"
+              description="Pharmacy chtbot using NLP"
+              ghLink="https://github.com/nimarta-devi/pharmacy-chatbot"
+              demoLink=""              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={fake_news}
+              isBlog={false}
+              title="Fake News Detection"
+              description="Fake News detection using Decision Tree"
+              ghLink="https://github.com/nimarta-devi/fake_news_detection"
+              demoLink=""              
+            />
+          </Col>
+
+
+
         </Row>
       </Container>
     </Container>
