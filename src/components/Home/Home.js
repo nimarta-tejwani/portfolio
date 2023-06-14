6ccc7fe7a83531a8e8db6b6d1261e0c62c67bcb5
Nimarta <nimarta.bcss19@iba-suk.edu.pk>
2023-06-14 23:38:18 +0500

    update about

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -32,12 +32,12 @@ function Home() {
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
+              {/* <img
                 src={homem}
                 alt="home pic"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
-              />
+              /> */}
             </Col>
           </Row>
         </Container>
